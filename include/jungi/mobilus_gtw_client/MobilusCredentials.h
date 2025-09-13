b27600d1202ea207f17c92acabab201eda9b4820
Piotr Kugla <piku235@gmail.com>
2025-09-13 13:56:37 +0200

    Add constructor to MobilusCredentials

--- include/jungi/mobilus_gtw_client/MobilusCredentials.h
+++ include/jungi/mobilus_gtw_client/MobilusCredentials.h
@@ -7,6 +7,12 @@ namespace jungi::mobilus_gtw_client {
 struct MobilusCredentials final {
     std::string username;
     std::string password;
+
+    MobilusCredentials(std::string aUsername, std::string aPassword)
+        : username(std::move(aUsername))
+        , password(std::move(aPassword))
+    {
+    }
 };
 
 }
