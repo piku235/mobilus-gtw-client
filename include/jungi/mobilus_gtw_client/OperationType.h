992fcc4972f62f6f8579856da8aaef3cd1c59a34
Piotr Kugla <piku235@gmail.com>
2025-10-18 14:23:32 +0200

    Add new protos: UpdateDeviceRequest and UpdateDeviceResponse

---
+++ include/jungi/mobilus_gtw_client/OperationType.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+
+namespace jungi::mobilus_gtw_client {
+
+namespace OperationType {
+    constexpr uint8_t Add = 0;
+    constexpr uint8_t Edit = 1;
+    constexpr uint8_t Delete = 2;
+}
+
+}
