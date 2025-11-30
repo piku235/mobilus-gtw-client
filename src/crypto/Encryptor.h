a33a4d76f17902cb4bf69aafa40af8c4bd97e469
Piotr Kugla <piku235@gmail.com>
2025-11-30 11:25:24 +0100

    Reorder methods in Encryptor

--- src/crypto/Encryptor.h
+++ src/crypto/Encryptor.h
@@ -6,9 +6,10 @@ namespace jungi::mobilus_gtw_client::crypto {
 
 class Encryptor {
 public:
+    virtual ~Encryptor() = default;
+
     virtual bytes encrypt(const bytes& plaintext, const bytes& key, const bytes& iv) const = 0;
     virtual bytes decrypt(const bytes& ciphertext, const bytes& key, const bytes& iv) const = 0;
-    virtual ~Encryptor() = default;
 };
 
 }
