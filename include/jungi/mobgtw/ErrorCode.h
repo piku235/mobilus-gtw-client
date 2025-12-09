7e2440447546407a91dce5b016ea7b063a025450
Piotr Kugla <piku235@gmail.com>
2025-12-09 19:11:23 +0100

    Drop integer values for ErrorCode

--- include/jungi/mobgtw/ErrorCode.h
+++ include/jungi/mobgtw/ErrorCode.h
@@ -1,23 +1,21 @@
 #pragma once
 
-#include <cstdint>
-
 namespace jungi::mobgtw {
 
-enum class ErrorCode : uint8_t {
-    InvalidSession = 1,
-    NoSession = 2,
-    LoginFailed = 3,
-    LoginTimeout = 4,
-    InvalidMessage = 5,
-    BadResponse = 6,
-    UnexpectedResponse = 7,
-    ResponseTimeout = 8,
-    Transport = 9,
-    NoConnection = 10,
-    ConnectionTimeout = 11,
-    ConnectionRefused = 12,
-    Unknown = 255,
+enum class ErrorCode {
+    InvalidSession,
+    NoSession,
+    LoginFailed,
+    LoginTimeout,
+    InvalidMessage,
+    BadResponse,
+    UnexpectedResponse,
+    ResponseTimeout,
+    Transport,
+    NoConnection,
+    ConnectionTimeout,
+    ConnectionRefused,
+    Unknown,
 };
 
 }
